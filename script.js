9b2fa36f5656101fae2516ac0bd77bdb5a0b6c4b
JulieKodehode <julie.kodehode@gmail.com>
2025-10-02 12:35:53 +0200

    Example

--- script.js
+++ script.js
@@ -14,39 +14,36 @@ function logInForm(event) {
     event.preventDefault(); // Ordet event er utgått men det funker
     console.log("Button clicked?");
 
-    // H
-    const inputEmail = document.querySelector("#inputEmail").value;
-    console.log(inputEmail);
-    const inputPassword = document.querySelector("#inputPassword").value;
-    console.log(inputPassword);
-    // Sjekkboks sjekk vil ikke funke uten en betingelse-sjekk (if/else statement)
-    //const inputShowPassword = document.querySelector("#inputShowPassword").value;
-    //console.log(inputShowPassword);
-
+    // Display Result
     const displayResult = document.querySelector("#displayResult");
     console.log(displayResult);
 
-    // L
+    // Input Email
+    const inputEmail = document.querySelector("#inputEmail").value;
+    console.log(inputEmail);
     const displayInputEmail = document.createElement("p");
+    displayInputEmail.textContent = inputEmail;
+    //displayResult.appendChild(displayInputEmail);
+
+    // Input Password
+    const inputPassword = document.querySelector("#inputPassword").value;
+    console.log(inputPassword);
     const displayInputPassword = document.createElement("p");
+    displayInputPassword.textContent = inputPassword;
+    //displayResult.appendChild(displayInputPassword);
+
+    // LogIn check
     const successfulLogIn = document.createElement("p");
     const failedLogIn = document.createElement("p");
 
-    // M
-    displayInputEmail.textContent = inputEmail;
-    displayInputPassword.textContent = inputPassword;
-    successfulLogIn.textContent = "Du har suksessfult logget inn på " + inputEmail;
+    successfulLogIn.textContent = `Du har suksessfult logget inn på ${inputEmail}.`
     failedLogIn.textContent = "Epost eller passord er feil."
 
-    // S
     if (inputEmail !== "" && inputPassword !== " ") {
         displayResult.appendChild(successfulLogIn);
     } else {
         displayResult.appendChild(failedLogIn);
     }
-
-    displayResult.appendChild(displayInputEmail);
-    displayResult.appendChild(displayInputPassword);
 }
 
 logInButton.addEventListener("click", logInForm);
